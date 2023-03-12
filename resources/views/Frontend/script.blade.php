f1f25a42edbe3526f9d9d4a0c6fc9f0abcde03b0
MohammedSalama <mohamed96salama@gmail.com>
2023-03-12 21:11:58 +0200

    Frontend-Theme

---
+++ resources/views/Frontend/script.blade.php
@@ -0,0 +1,15 @@
+<!-- Vendor JS Files -->
+<script src="{{ URL::asset('Frontend/assets/vendor/jquery/jquery.min.js') }}"></script>
+<script src="{{ URL::asset('Frontend/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ URL::asset('Frontend/assets/vendor/jquery.easing/jquery.easing.min.js') }}"></script>
+<script src="{{ URL::asset('Frontend/assets/vendor/php-email-form/validate.js') }}"></script>
+<script src="{{ URL::asset('Frontend/assets/vendor/waypoints/jquery.waypoints.min.js') }}"></script>
+<script src="{{ URL::asset('Frontend/assets/vendor/counterup/counterup.min.js') }}"></script>
+<script src="{{ URL::asset('Frontend/assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+<script src="{{ URL::asset('Frontend/assets/vendor/venobox/venobox.min.js') }}"></script>
+<script src="{{ URL::asset('Frontend/assets/vendor/owl.carousel/owl.carousel.min.js') }}"></script>
+<script src="{{ URL::asset('Frontend/assets/vendor/typed.js/typed.min.js') }}"></script>
+<script src="{{ URL::asset('Frontend/assets/vendor/aos/aos.js') }}"></script>
+
+<!-- Template Main JS File -->
+<script src="{{ URL::asset('Frontend/assets/js/main.js') }}"></script>
